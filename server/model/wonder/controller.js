85110b706e882fbe87960d46e666971c04eb97ca
Amimaro <amir.zahlan@gmail.com>
2018-03-12 05:42:15 -0300

    Update create method

--- server/model/wonder/controller.js
+++ server/model/wonder/controller.js
@@ -1,6 +1,19 @@
 const Controller = require('../../lib/controller');
 const wonder = require('./facade');
 
-class Wonder extends Controller {}
+class Wonder extends Controller {
+
+  create(req, res, next) {
+    if (req.isAuthenticated()) {
+      req.body.owner = req.user._id
+      this.facade.create(req.body)
+        .then(doc => res.status(201).json(doc))
+        .catch(err => next(err));
+    } else {
+      res.sendStatus(401);
+    }
+  }
+
+}
 
 module.exports = new Wonder(wonder);
