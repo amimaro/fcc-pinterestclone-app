a993a58fbcea72edc97f307b67bc9f7ac80380f3
Amimaro <amir.zahlan@gmail.com>
2018-03-12 05:38:00 -0300

    Update wonder schema

--- server/model/wonder/schema.js
+++ server/model/wonder/schema.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const wonder = new Schema({
   title: { type: String, required: true },
-  body: { type: String }
+  link: { type: String },
+  hashtags: { type: String }
 });
 
 
