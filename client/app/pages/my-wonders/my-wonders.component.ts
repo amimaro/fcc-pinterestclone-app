fa445967c7c3ca3ce0ed4a75237f46c223843fa9
Amimaro <amir.zahlan@gmail.com>
2018-03-11 21:03:18 -0300

    Add wonder modal

--- client/app/pages/my-wonders/my-wonders.component.ts
+++ client/app/pages/my-wonders/my-wonders.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyWondersComponent implements OnInit {
 
-  constructor() { }
+  isModalActive: boolean = true;
+  form: any = {
+    title: 'Wonderful',
+    link: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Korb_mit_Br%C3%B6tchen.JPG'
+  };
+  imgSrc: string = 'https://bulma.io/images/placeholders/128x128.png';
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+  }
+
+  toggleModal() {
+    this.isModalActive = !this.isModalActive;
+  }
+
+  updateUrl(e) {
+    this.imgSrc = 'https://bulma.io/images/placeholders/128x128.png';
+  }
+
+  updateImage() {
+    this.imgSrc = this.form.link;
   }
 
 }
