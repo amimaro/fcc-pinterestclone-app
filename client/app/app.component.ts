9e032accbedeff988d7bc1438d4d1a9d6074f197
Amimaro <amir.zahlan@gmail.com>
2018-03-18 08:46:23 -0300

    Change background image

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -11,10 +11,25 @@ export class AppComponent {
   title = 'app';
   isNavBar: boolean = false;
 
-  constructor(public appService: AppService) { }
+  constructor(public appService: AppService) {
+    this.changeBackground();
+  }
 
   toggleNavBar() {
     return this.isNavBar;
+  }
+
+  changeBackground() {
+    let images = [
+      'assets/background/milky-way-2695569_1920.jpg',
+      'assets/background/desert-1654439_1920.jpg',
+      'assets/background/desert-2340326_1920.jpg',
+      'assets/background/polynesia-3021072_1920.jpg'
+    ];
+    document.body.style.background = `url(${images[Math.floor(Math.random() * 4)]})`;
+    setInterval(() => {
+      document.body.style.background = `url(${images[Math.floor(Math.random() * 4)]})`;
+    }, 60000);
   }
 
 }
